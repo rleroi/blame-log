b541c5570e988e319af4296eabfd2541384ef4db
R. Leroi <r.leroi@live.nl>
2023-12-15 13:34:24 +0100

    refactor

--- src/BlameLog.php
+++ src/BlameLog.php
@@ -2,32 +2,35 @@
 
 namespace RLeroi\BlameLog;
 
-class BlameLog {
+class BlameLog
+{
+    private const LOG_REGEX = '/at (.+\.php):([0-9]+)\)/i';
+    private const BLAME_REGEX = '/[0-9a-fA-F]+ \(([a-z ]+) [0-9]{4}-[0-9\-+ :a-z]+\)/i';
+
     public static function calculateTotals(string $logFile): array
     {
         $logs = fopen($logFile, 'r');
         $names = [];
-        while($log = fgets($logs)) {
+        while ($log = fgets($logs)) {
             $matches = [];
-            preg_match('/at (.+\.php):([0-9]+)\)/i', $log, $matches);
+            preg_match(self::LOG_REGEX, $log, $matches);
             if (count($matches) < 3) {
                 continue;
             }
             [$match, $file, $line] = $matches;
             $dir = dirname($file);
-            $resultCode = 0;
-            $blame = exec("cd $dir && git blame -L $line,$line $file 2>&1", result_code: $resultCode);
-            if ($resultCode) {
+            $errorCode = 0;
+            $blame = self::runGitBlame($dir, $file, $line, $errorCode);
+            if ($errorCode) {
                 continue;
             }
 
-            preg_match('/[0-9a-fA-F]+ \(([a-z ]+) [0-9]{4}-[0-9\-+ :a-z]+\)/i', $blame, $matches);
+            preg_match(self::BLAME_REGEX, $blame, $matches);
             if (count($matches) < 2) {
                 continue;
             }
 
             $name = array_pop($matches);
-
             if (!isset($names[$name])) {
                 $names[$name] = 0;
             }
@@ -36,5 +39,14 @@ class BlameLog {
         fclose($logs);
 
         return $names;
+    }
+
+    private static function runGitBlame(string $dir, string $file, int $line, int &$errorCode): string
+    {
+        $command = "cd $dir && git blame -L $line,$line $file 2>&1";
+        $output = [];
+        exec($command, $output, $errorCode);
+
+        return implode(PHP_EOL, $output);
     }
 }
